58956251f4a9e57afe092964b46b7e2213d5b952
ramons <ramoncssantos@gmail.com>
2019-08-02 21:44:11 -0300

    example to team

---
+++ WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IO;
+using WebApplication1.Helpers;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            Person a = Serialize();
+
+            return JsonConvert.SerializeObject(a);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+
+        private Person Serialize()
+        {
+            Serializer ser = new Serializer();
+            string path = string.Empty;
+            string xmlInputData = string.Empty;
+            string xmlOutputData = string.Empty;
+
+
+
+            path = Directory.GetCurrentDirectory() + @"\Data\XMLFile.xml";
+            xmlInputData = ser.Teste(path);
+
+            Person pessoa = ser.Deserialize<Person>(xmlInputData);
+            //xmlOutputData = ser.Serialize<Pessoa>(pessoa);
+
+            return pessoa;
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WebApplication1.Models
+{
+    [Serializable, XmlRoot("person")]
+    public class Person
+    {
+        public string description { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public List<Person> ListPerson { get; set; }
+
+        public Person()
+        {
+            this.ListPerson = new List<Person>();
+        }
+    }
+}
+
