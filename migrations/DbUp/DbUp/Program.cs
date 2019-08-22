6e441fedec81c8d346a70b2bc5f29393cf5530e8
ramons <ramoncssantos@gmail.com>
2019-08-22 00:26:15 -0300

    dbup

---
+++ migrations/DbUp/DbUp/Program.cs
@@ -0,0 +1,38 @@
+﻿using DbUp;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace DbUpDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var connectionString =
+                args.FirstOrDefault()
+                ?? "Data Source=localhost;Initial Catalog=Mocks;User ID=rootmock;Password=123456abc@";
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ migrations/MigrationsFluent/MigrationsFluent/Migrations/insere_data.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MigrationsFluent.Migrations
+{
+    [Migration(88896, "esqueci do produto")]
+    public class insere_data : Migration
+    {
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Up()
+        {
+            IfDatabase("SqlServer")
+                .Execute.Sql("INSERT INTO Produtos Values ('bisnaguinha')");
+        }
+    }
+}
